a2eba55f17102d7e8006c48e0a744f52c7157b8b
Eddyel <eddyel.yam@gmail.com>
2023-09-09 15:01:50 -0600

    Nuevos cambios e inicio

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirecciona a '/home' en la raíz
+  { path: 'home', component: HomeComponent }, // Ruta para el componente "home"
+  // Agrega otras rutas según sea necesario
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
