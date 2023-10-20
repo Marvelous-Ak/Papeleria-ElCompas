9c1c676e81c6d1a826645c03b9f4d7b4cf7c8793
Eddyel <eddyel.yam@gmail.com>
2023-10-20 14:04:21 -0600

    Actualizacion de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
 const routes: Routes = [
-  {path: "home", loadChildren:()=>import('./home/home.module').then(m=>m.HomeModule)}
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: SkeletonComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./pages/pages-routing.module').then((m) => m.PagesRoutingModule)
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SkeletonComponent } from './layout/skeleton/skeleton.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    FooterComponent,
+    NavbarComponent,
+    SkeletonComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
