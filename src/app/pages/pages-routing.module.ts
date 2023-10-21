dd36e351e79cff5682c4f88a01123098c16fe464
Eddyel <eddyel.yam@gmail.com>
2023-10-21 13:09:15 -0600

    Search

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
-import { CommentComponent } from './comment/comment.component';
 import { LocationComponent } from './location/location.component';
 import { SearchComponent } from './search/search.component';
 import { ProductComponent } from './product/product.component';
+
+//Prueba de diseños
+import { CardComponent } from './card/card.component';
+import { CommentComponent } from './comment/comment.component';
 
 const routes: Routes = [
   {
@@ -15,10 +18,6 @@ const routes: Routes = [
   {
     path: 'catalog/:numero',
     component: CatalogComponent,
-  },
-  {
-    path: 'comment',
-    component: CommentComponent,
   },
   {
     path: 'location',
@@ -31,6 +30,16 @@ const routes: Routes = [
   {
     path: 'product/:numero',
     component: ProductComponent,
+  },
+
+  //Prueba de diseños
+  {
+    path: 'comment',
+    component: CommentComponent,
+  },
+  {
+    path: 'card',
+    component: CardComponent,
   },
 ];
 
