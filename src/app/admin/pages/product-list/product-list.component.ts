d6da5ddbb41987dd257fb088c20f5a7a126ad8c0
Andres7027 <>
2023-10-28 12:44:25 -0600

    Consumo del API en la lista de productos, funcionamiento de paginación

--- src/app/admin/pages/product-list/product-list.component.ts
+++ src/app/admin/pages/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Product } from 'src/app/Shared/data';
+import { CatalogosService } from 'src/app/Services/catalogs.service';
 
 @Component({
   selector: 'app-product-list',
@@ -6,5 +8,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent {
+  public listProduct: Product []=[];
 
+  itemsPerPage: number = 10; // Cantidad de productos por página
+  currentPage: number = 1; // Página actual
+
+  constructor( private CatalogoService:CatalogosService){
+  }
+  ngOnInit(){
+    this.listAll();
+    this.getPages();
+  }
+
+
+  //// Traer todos los productos: función ShowAll
+  public listAll(){ 
+    this.CatalogoService.get('http://127.0.0.1:8000/api/catalogos')
+    .subscribe((respuesta: any) => {
+      this.listProduct = respuesta;
+    })
+  }
+
+  //Paginación
+  getTotalPages(): number {
+    return Math.ceil(this.listProduct.length / this.itemsPerPage);
+  }
+  
+  getPages(): number[] {
+    return Array(this.getTotalPages())
+      .fill(0)
+      .map((x, i) => i + 1);
+  }
+  
 }
