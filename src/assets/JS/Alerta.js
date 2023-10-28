ab6c448f974c0727e7da2c44af1f110aaf398a1b
RODF10 <blueapp2015@hotmail.com>
2023-10-27 22:35:51 -0600

    Cambio click

--- src/assets/JS/Alerta.js
+++ src/assets/JS/Alerta.js
@@ -5,3 +5,11 @@ function mialerta(){
         text: 'Inicie Sesion o Crea una cuenta para continuar',
       })
 }
+
+function addFavorites(){
+  Swal.fire({
+    icon: 'warning',
+    title: 'Inicie Sesion',
+    text: 'Inicie Sesion o Crea una cuenta para añador a favoritos',
+  })
+}
