2076cf3171f35a6acbe24cb34e1c3ae1acf58a71
RODF10 <blueapp2015@hotmail.com>
2023-10-30 16:01:25 -0600

    Correcion del apartado Promocion

--- src/assets/JS/ValidacionProductAdd.js
+++ src/assets/JS/ValidacionProductAdd.js
@@ -1,13 +1,17 @@
 var checkbox = document.getElementById('flexSwitchCheckDefault');
+var promoAct = document.getElementById('desc2');
 checkbox.addEventListener("change", validaCheckbox, false);
   
 function validaCheckbox(){
   var checked = checkbox.checked;
   if(checked){
+    console.log('verdadero');
     //alert('checkbox esta seleccionado');
-    document.querySelector('.actPromo').style.display = 'block';
+    // document.querySelector('.actPromo').style.display = 'block';
   } else {
-    document.querySelector('.actPromo').style.display = 'none';
+    console.log('False');
+    // document.querySelector('.actPromo').style.display = 'none';
 
   }
+  
 }
