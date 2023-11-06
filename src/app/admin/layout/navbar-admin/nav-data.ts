bda52bd1771ee36ec182710cb2a3c93b6d89b213
Eddyel <eddyel.yam@gmail.com>
2023-11-06 13:15:44 -0600

    admin secciones

--- src/app/admin/layout/navbar-admin/nav-data.ts
+++ src/app/admin/layout/navbar-admin/nav-data.ts
@@ -1,12 +1,27 @@
 export const navbarData = [
-{
+  {
     routeLink: 'Dashboard',
     icon: 'fa-solid fa-house',
-    label: 'Dashboard'
-},
-{
+    label: 'Dashboard',
+  },
+  {
+    routeLink: '',
+    icon: 'fa-solid fa-home-user',
+    label: 'Home',
+  },
+  {
     routeLink: 'Product-List',
-    icon: 'fa-solid fa-box-open',
-    label: 'Lista de Productos'
-},
+    icon: 'fa-solid fa-cart-shopping',
+    label: 'Lista de Productos',
+  },
+  {
+    routeLink: '',
+    icon: 'fa-solid fa-boxes-stacked',
+    label: 'Bodega (Sin Funcion)',
+  },
+  {
+    routeLink: '',
+    icon: 'fa-regular fa-address-card',
+    label: 'Proveedores (Sin Funcion)',
+  },
 ];
